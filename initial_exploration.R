7089b13132b612cf75e76d5999d9c6844bdd2b8b
Harry Allen <harry.allen@dft.gov.uk>
2026-01-14 15:22:52 +0000

    Connecting to gcp

--- initial_exploration.R
+++ initial_exploration.R
@@ -1,1 +1,13 @@
 library(jsonencryptor)
+library(bigrquery)
+library(DBI)
+
+##Authenticate for BQ connection
+bigrquery::bq_auth(path = jsonencryptor::secret_read("service_secret.json"))
+
+##Connect to big query
+con <- DBI::dbConnect(
+  bigrquery::bigquery(),
+  project = "dft-gcp-mobilenetworkdata-prod", 
+  dataset = "mobilenetwork_data_test"
+)
