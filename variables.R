c0c90365bf4d63159afc9a78250dc6032a570c65
Harry Allen <harry.allen@dft.gov.uk>
2026-02-04 10:43:55 +0000

    Adding file of variables to ensure reproducability

---
+++ variables.R
@@ -0,0 +1,21 @@
+## Setting variables so they're not hard coded
+
+# Baseline dates
+baseline_start <- "2023-03-06"
+baseline_end <- "2023-03-12"
+
+# Strike dates
+strike_date_1 <- "2023-03-15"
+strike_date_2 <- "2023-03-16"
+strike_date_3 <- "2023-03-18"
+strike_date_4 <- "2023-05-12"
+strike_date_5 <- "2023-05-13"
+strike_date_6 <- "2023-03-30"
+strike_date_7 <- "2023-04-01"
+
+# Bank holidays
+bank_hol_date_1 <- "2023-04-07"
+bank_hol_date_2 <- "2023-04-10"
+bank_hol_date_1 <- "2023-05-01"
+bank_hol_date_2 <- "2023-05-08"
+bank_hol_date_3 <- "2023-05-29"
