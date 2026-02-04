589d28fc2792b0aebb5b49d5dbdd1e57d1698bb7
Harry Allen <harry.allen@dft.gov.uk>
2026-02-04 12:37:46 +0000

    Developing initial chart

--- strike_analysis.R
+++ strike_analysis.R
@@ -51,3 +51,26 @@ waterloo_with_baseline <- strike_data %>%
                 residentSum_perc = residentSum / resident_count_baseline,
                 workerSum_perc = workerSum / worker_count_baseline,
                 visitorSum_perc = visitorSum / visitor_count_baseline)
+
+ggplot(data = waterloo_with_baseline, aes(x = date))+
+  geom_line(aes(y = residentSum_perc),
+            colour = "#004D3B",
+            linewidth = 1)+
+  geom_line(aes(y = workerSum_perc),
+            colour = "#3C9F8B",
+            linewidth = 1)+
+  geom_line(aes(y = visitorSum_perc),
+            colour = "#001A70",
+            linewidth = 1)+
+  scale_y_continuous(labels = scales::label_percent(),
+                     limits = c(0, 1.5))+
+  ggplot2::theme(panel.background = ggplot2::element_rect(fill = "white"),
+                 panel.grid.major.y = ggplot2::element_line(colour = "grey", linewidth = 0.1),
+                 strip.background = ggplot2::element_rect(fill = "white"),
+                 axis.line.x = ggplot2::element_line(colour = "black", linewidth = 1),
+                 axis.line.y = ggplot2::element_line(colour = "black", linewidth = 1))+
+  geom_vline(xintercept = all_strike_date,
+             colour = "grey")+
+  geom_hline(yintercept = 1,
+             colour = "black",
+             size = 1)
