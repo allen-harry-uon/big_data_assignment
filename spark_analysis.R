a2b4b9738d52ba5eafe246632e3f27347a3091fe
Harry Allen <harry.allen@dft.gov.uk>
2026-02-04 16:46:31 +0000

    Specifying column types for writing to Spark

--- spark_analysis.R
+++ spark_analysis.R
@@ -7,3 +7,13 @@ sparklyr::spark_installed_versions()
 
 # Connect to local cluster
 sc <- sparklyr::spark_connect(master = "local", version = "4.0.1")
+
+column_types <- c(date = "Date",
+                  time = "POSIXct",
+                  msoa = "character",
+                  peopleCount = "integer",
+                  residentSum = "integer",
+                  workerSum = "integer",
+                  visitorSum = "integer")
+
+waterloo_data_sc <- sparklyr::spark_read_csv(sc, "Data/waterloo_table.csv", columns = column_types)
