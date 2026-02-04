dec12c1c1e2c3aa5acbd37a32b6d1a5db6169bc1
Harry Allen <harry.allen@dft.gov.uk>
2026-02-04 12:12:22 +0000

    Finishing comparison with baseline

--- strike_analysis.R
+++ strike_analysis.R
@@ -3,6 +3,7 @@ library(bigrquery)
 library(DBI)
 library(dplyr)
 library(lubridate)
+library(ggplot2)
 
 ##Authenticate for BQ connection
 bigrquery::bq_auth(path = jsonencryptor::secret_read("service_secret.json"))
@@ -24,10 +25,16 @@ waterloo_data <- DBI::dbGetQuery(con, "SELECT date,
                                      WHERE msoa = 'E02006801'")
 
 strike_data <- waterloo_data %>% 
-  dplyr::mutate(weekday = lubridate::wday(date, week_start = 1)) %>% 
   dplyr::filter(hms::as_hms(time) >= hms("08:00:00"),
-                hms::as_hms(time) <= hms("19:00:00"),
-                between(weekday, 1, 5))
+                hms::as_hms(time) <= hms("19:00:00")) %>% 
+  dplyr::group_by(date, msoa) %>% 
+  dplyr::summarise(peopleCount = mean(peopleCount),
+                   residentSum = mean(residentSum),
+                   workerSum = mean(workerSum),
+                   visitorSum = mean(visitorSum)) %>% 
+  dplyr::mutate(weekday = lubridate::wday(date, week_start = 1)) %>% 
+  dplyr::filter(between(weekday, 1, 5)) %>% 
+  dplyr::ungroup()
 
 baseline <- strike_data %>% 
   dplyr::filter(date >= baseline_start & date <= baseline_end) %>% 
@@ -38,7 +45,7 @@ baseline <- strike_data %>%
                 visitor_count_baseline = visitorSum)
 
 waterloo_with_baseline <- strike_data %>% 
-  dplyr::left_join(baseline, by = join_by(weekday, time, msoa)) %>% 
+  dplyr::left_join(baseline, by = join_by(weekday, msoa)) %>% 
   dplyr::filter(date >= "2023-03-13") %>% 
   dplyr::mutate(peopleCount_perc = peopleCount / people_count_baseline,
                 residentSum_perc = residentSum / resident_count_baseline,
