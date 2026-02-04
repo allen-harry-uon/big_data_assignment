570fae36fa82daffa20902eec99c9885e78224f6
Harry Allen <harry.allen@dft.gov.uk>
2026-02-04 12:37:31 +0000

    Creating vectors of all dates

--- variables.R
+++ variables.R
@@ -13,9 +13,18 @@ strike_date_5 <- "2023-05-13"
 strike_date_6 <- "2023-03-30"
 strike_date_7 <- "2023-04-01"
 
+all_strike_date <- c(strike_date_1,strike_date_2, strike_date_3,
+                     strike_date_4, strike_date_5, strike_date_6,
+                     strike_date_7) %>% 
+  purrr::map_vec(as.Date)
+
 # Bank holidays
 bank_hol_date_1 <- "2023-04-07"
 bank_hol_date_2 <- "2023-04-10"
-bank_hol_date_1 <- "2023-05-01"
-bank_hol_date_2 <- "2023-05-08"
-bank_hol_date_3 <- "2023-05-29"
+bank_hol_date_3 <- "2023-05-01"
+bank_hol_date_4 <- "2023-05-08"
+bank_hol_date_5 <- "2023-05-29"
+
+all_bank_hols <- c(bank_hol_date_1, bank_hol_date_2, bank_hol_date_3,
+                   bank_hol_date_4, bank_hol_date_5) %>% 
+  purrr::map_vec(as.Date)
