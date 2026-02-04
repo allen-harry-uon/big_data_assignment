81609e7b072648d7deed8c3ba07ed78bb3e672d7
Harry Allen <harry.allen@dft.gov.uk>
2026-02-04 11:46:43 +0000

    Combining strike data with baseline

--- strike_analysis.R
+++ strike_analysis.R
@@ -2,6 +2,7 @@ library(jsonencryptor)
 library(bigrquery)
 library(DBI)
 library(dplyr)
+library(lubridate)
 
 ##Authenticate for BQ connection
 bigrquery::bq_auth(path = jsonencryptor::secret_read("service_secret.json"))
@@ -23,10 +24,9 @@ waterloo_data <- DBI::dbGetQuery(con, "SELECT date,
                                      WHERE msoa = 'E02006801'")
 
 strike_data <- waterloo_data %>% 
-  dplyr::mutate(time = hms::as_hms(time),
-                weekday = lubridate::wday(date, week_start = 1)) %>% 
-  dplyr::filter(time >= hms("08:00:00"),
-                time <= hms("19:00:00"),
+  dplyr::mutate(weekday = lubridate::wday(date, week_start = 1)) %>% 
+  dplyr::filter(hms::as_hms(time) >= hms("08:00:00"),
+                hms::as_hms(time) <= hms("19:00:00"),
                 between(weekday, 1, 5))
 
 baseline <- strike_data %>% 
@@ -36,3 +36,11 @@ baseline <- strike_data %>%
                 resident_count_baseline = residentSum,
                 worker_count_baseline = workerSum,
                 visitor_count_baseline = visitorSum)
+
+waterloo_with_baseline <- strike_data %>% 
+  dplyr::left_join(baseline, by = join_by(weekday, time, msoa)) %>% 
+  dplyr::filter(date >= "2023-03-13") %>% 
+  dplyr::mutate(peopleCount_perc = peopleCount / people_count_baseline,
+                residentSum_perc = residentSum / resident_count_baseline,
+                workerSum_perc = workerSum / worker_count_baseline,
+                visitorSum_perc = visitorSum / visitor_count_baseline)
