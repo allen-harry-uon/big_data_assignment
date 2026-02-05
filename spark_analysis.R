b912913bcd86c2f157aee9238c78a6e3eeafe5af
Harry Allen <harry.allen@dft.gov.uk>
2026-02-05 14:44:49 +0000

    Converting analysis to spark objects

--- spark_analysis.R
+++ spark_analysis.R
@@ -1,6 +1,8 @@
 library(sparklyr)
+library(dplyr)
+library(ggplot2)
 
-# Run once to install spark
+  # Run once to install spark
 # sparklyr::spark_install(version = "4.0.1")
 # Run to check spark installed correctly 
 sparklyr::spark_installed_versions()
@@ -33,6 +35,68 @@ baseline_sc <- sparklyr::spark_read_csv(sc,
                                         name = "baseline",
                                         path = "Data/crowd_data/baseline.csv",
                                         columns = baseline_column_types)
+
+strike_data_sc <- waterloo_data_sc %>% 
+  dplyr::mutate(time = date_format(time, "HH:mm:ss")) %>% 
+  dplyr::filter(time >= "08:00:00",
+                time <= "19:00:00") %>% 
+  dplyr::group_by(date, msoa) %>% 
+  dplyr::summarise(peopleCount = mean(peopleCount, na.rm = TRUE),
+                   residentSum = mean(residentSum, na.rm = TRUE),
+                   workerSum = mean(workerSum, na.rm = TRUE),
+                   visitorSum = mean(visitorSum, na.rm = TRUE)) %>% 
+  dplyr::mutate(weekday = dayofweek(date)) %>% 
+  dplyr::filter(between(weekday, 2, 6)) %>% 
+  dplyr::ungroup() %>% 
+  dplyr::left_join(baseline_sc, by = join_by(weekday, msoa)) %>% 
+  dplyr::filter(date >= "2023-03-13") %>% 
+  dplyr::mutate(peopleCount_perc = peopleCount / people_count_baseline,
+                residentSum_perc = residentSum / resident_count_baseline,
+                workerSum_perc = workerSum / worker_count_baseline,
+                visitorSum_perc = visitorSum / visitor_count_baseline) %>% 
+  dplyr::select(date, residentSum_perc, workerSum_perc, 
+                visitorSum_perc) %>% 
+  tidyr::pivot_longer(cols = c(residentSum_perc, workerSum_perc, 
+                               visitorSum_perc),
+                      names_to = "travel_reason",
+                      values_to = "perc") %>% 
+  sparklyr::sdf_register(name = "strike_data")
+
+strike_data_to_plot <- strike_data_sc %>% 
+  sparklyr::collect()
+
+ggplot(data = strike_data_to_plot, aes(x = date,
+                                  y = perc,
+                                  group = travel_reason,
+                                  colour = travel_reason))+
+  geom_line(linewidth = 1)+
+  scale_colour_manual(values = palette)+
+  scale_y_continuous(labels = scales::label_percent(),
+                     limits = c(0, 1.5),
+                     name = "")+
+  scale_x_date(name = "Date")+
+  ggplot2::theme(panel.background = ggplot2::element_rect(fill = "white"),
+                 panel.grid.major.y = ggplot2::element_line(colour = "grey", linewidth = 0.1),
+                 strip.background = ggplot2::element_rect(fill = "white"),
+                 axis.line.x = ggplot2::element_line(colour = "black", linewidth = 1),
+                 axis.line.y = ggplot2::element_line(colour = "black", linewidth = 1))+
+  geom_vline(xintercept = all_strike_date,
+             colour = "grey")+
+  geom_vline(xintercept = all_bank_hols,
+             colour = "grey",
+             linetype = "dashed")+
+  geom_hline(yintercept = 1,
+             colour = "black",
+             size = 1)+
+  annotate("label",
+           label = "Solid line =\n Train strikes",
+           x = as.Date("2023-05-21"),
+           y = 1.5)+
+  annotate("label",
+           label = "Dashed line =\n Bank holidays",
+           x = as.Date("2023-05-21"),
+           y = 1.35)+
+  labs(colour = "Reason for travel")
 
 # When finished using Spark
 spark_disconnect(sc)
