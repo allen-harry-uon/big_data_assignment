3233dad060cbc089edc382ed3c0a0df47245ef55
Harry Allen <harry.allen@dft.gov.uk>
2026-02-05 10:46:33 +0000

    Adding baseline to spark context

--- spark_analysis.R
+++ spark_analysis.R
@@ -1,13 +1,14 @@
 library(sparklyr)
 
 # Run once to install spark
-sparklyr::spark_install(version = "4.0.1")
+# sparklyr::spark_install(version = "4.0.1")
 # Run to check spark installed correctly 
 sparklyr::spark_installed_versions()
 
 # Connect to local cluster
 sc <- sparklyr::spark_connect(master = "local", version = "4.0.1")
 
+# Specifying column types to allow for faster reading into Spark
 column_types <- c(date = "Date",
                   time = "POSIXct",
                   msoa = "character",
@@ -16,10 +17,22 @@ column_types <- c(date = "Date",
                   workerSum = "integer",
                   visitorSum = "integer")
 
+baseline_column_types <- c(msoa = "character",
+                           peopleCount = "double",
+                           residentSum = "double",
+                           workerSum = "double",
+                           visitorSum = "double",
+                           weekday = "integer")
+
 waterloo_data_sc <- sparklyr::spark_read_csv(sc, 
                                              name = "waterloo_data",
                                              path = "Data/crowd_data/waterloo_table.csv", 
                                              columns = column_types)
 
-waterloo_data_sc %>% 
-  dplyr::mutate(time = date_format(time, "HH:mm:ss"))
+baseline_sc <- sparklyr::spark_read_csv(sc,
+                                        name = "baseline",
+                                        path = "Data/crowd_data/baseline.csv",
+                                        columns = baseline_column_types)
+
+# When finished using Spark
+spark_disconnect(sc)
