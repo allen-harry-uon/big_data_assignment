17fd5e3541be667c49e74bdb4c8dd9f34d8f5b01
Harry Allen <harry.allen@dft.gov.uk>
2026-02-10 17:34:42 +0000

    Commenting on initial exploration

--- exploration_files/initial_exploration.R
+++ exploration_files/initial_exploration.R
@@ -16,13 +16,16 @@ con <- DBI::dbConnect(
   dataset = "crowd_monitoring_api"
 )
 
+# Reading in shapefiles
 msoa_shapefile <- sf::read_sf("Data/map/Middle_layer_Super_Output_Areas_(December_2021)_Boundaries_EW_BFE_(V8)_and_RUC.shp")
 
 waterloo_shape <- msoa_shapefile %>% 
-  dplyr::filter(MSOA21CD == "E02006801") %>% 
+  dplyr::filter(MSOA21CD == waterloo_msoa) %>% 
   sf::st_as_sf() %>% 
+  # Making sure shape is properly projected on to world map
   sf::st_transform(crs = "+proj=longlat +datum=WGS84")
 
+# Getting sample of data
 waterloo_data <- DBI::dbGetQuery(con, "SELECT date, 
                                               time, 
                                               peopleCount, 
@@ -34,20 +37,25 @@ waterloo_data <- DBI::dbGetQuery(con, "SELECT date,
                                        WHERE date = DATE '2023-05-17'
                                        AND msoa = 'E02006801'")
 
+# Joining shape data to crowd data
 waterloo_mapped_data <- waterloo_data %>% 
   dplyr::group_by(date, msoa) %>% 
+  # Calculating total count per day
   dplyr::summarise(total_count = sum(peopleCount)) %>% 
   dplyr::left_join(waterloo_shape, by = join_by(msoa == MSOA21CD)) %>% 
   sf::st_as_sf() %>% 
   sf::st_transform(crs = "+proj=longlat +datum=WGS84")
 
+# Formatting label when hovering over shape
 location_label <- sprintf("<span style='font-weight:bold; font-size:1.5em;'>%s</span>", waterloo_mapped_data$MSOA21NM)
 count_label <- sprintf("<br>Count: %s", waterloo_mapped_data$total_count)
 
 labels <- paste(location_label, count_label) %>% 
   lapply(htmltools::HTML)
 
+# Creating map
 leaflet::leaflet() %>% 
+  # Adding world map underneath
   addTiles(options = tileOptions(opacity = 0.5)) %>% 
   addPolygons(data = waterloo_mapped_data,
               weight = 3,
